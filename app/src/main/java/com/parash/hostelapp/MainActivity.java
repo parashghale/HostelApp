1329834d2fb429cacff80ec255017b8b56bc77de
parashghale <parash.ghale20@gmail.com>
2020-02-16 01:53:53 +0545

    Login and signup completed

--- app/src/main/java/com/parash/hostelapp/MainActivity.java
+++ app/src/main/java/com/parash/hostelapp/MainActivity.java
@@ -1,14 +1,54 @@
 package com.parash.hostelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseAuth firebaseAuth;
+    EditText etUsername,etpassword;
+    Button btnsignup,btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = firebaseAuth.getInstance();
+        etUsername=findViewById(R.id.etusename);
+        etpassword=findViewById(R.id.etpassword);
+        btnsignup=findViewById(R.id.btnSignup);
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnlogin=findViewById(R.id.login);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signInWithEmailAndPassword(etUsername.getText().toString(),etpassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Intent intent = new Intent(getApplicationContext(),DashboardActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
